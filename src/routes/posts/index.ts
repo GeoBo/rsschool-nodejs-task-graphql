07b47ae1d31fb4edb4fb3b90a217afd0ab2d8fc6
Geobo <bozhys@gmail.com>
2023-01-26 22:02:56 +0300

    feat: add member-types, posts endpoints

--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -1,3 +1,4 @@
+import { HttpError } from '@fastify/sensible/lib/httpError';
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { createPostBodySchema, changePostBodySchema } from './schema';
@@ -6,7 +7,9 @@ import type { PostEntity } from '../../utils/DB/entities/DBPosts';
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<PostEntity[]> {});
+  fastify.get('/', async function (request, reply): Promise<PostEntity[]> {
+    return fastify.db.posts.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -15,9 +18,20 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity | HttpError> {
+      const id = request.params.id;
+      try {
+        const post = await fastify.db.posts.findOne({key: 'id', equals: id });
+        if(!post) throw new Error(`Post with id=${id} not exist`);
+        return post;
+      } catch(err) {
+        if (err instanceof Error) return fastify.httpErrors.notFound(err.message);
+        return fastify.httpErrors.internalServerError();
+      }
+    }
   );
-
+  
+  //Проверить сущестовование пользователя, написавшего пост
   fastify.post(
     '/',
     {
@@ -25,7 +39,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: createPostBodySchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity | HttpError> {
+      const postBody = request.body;
+      try {
+        const post = await fastify.db.posts.create(postBody);
+        if(!post) throw new Error(`User not created`);
+        reply.code(201);
+        return post;
+      } catch(err) {
+          if (err instanceof Error) return fastify.httpErrors.badRequest(err.message);
+          return fastify.httpErrors.internalServerError();
+      }}
   );
 
   fastify.delete(
@@ -35,7 +59,19 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity | HttpError> {
+      const id = request.params.id;
+      try {
+        const post = await fastify.db.posts.delete(id);
+        if(!post) throw new Error(`Post with id=${id} not exist`);
+        return post;   
+      } catch(err) {
+        if (err instanceof Error) {
+          return fastify.httpErrors.notFound(err.message);
+        } 
+        return fastify.httpErrors.internalServerError();
+      }
+    }
   );
 
   fastify.patch(
@@ -46,7 +82,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity | HttpError> {
+      const id = request.params.id;
+      const postBody = request.body;
+
+      try {
+        return fastify.db.posts.change(id, postBody);
+      } catch(err) {
+          if (err instanceof Error) return fastify.httpErrors.badRequest(err.message);
+          return fastify.httpErrors.internalServerError();
+      }
+    }
   );
 };
 
